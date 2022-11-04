9fca7b8427314f3dc3e0af10a1a500b901fdcae7
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-11-04 17:16:01 -0300

    Requisito 4 ok

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 const colors = document.querySelectorAll('.color');
 colors[0].style.background = 'black';
 
+const button = document.createElement('button');
+button.id = 'button-random-color';
+button.innerText = 'Cores aleatórias';
+
+const header = document.getElementsByTagName('header')[0];
+header.appendChild(button);
+
 const colorGenerate = () => {
   const red = parseInt(Math.random() * 255);
   const green = parseInt(Math.random() * 255);
@@ -12,6 +19,12 @@ const colorGenerate = () => {
 
 console.log(colorGenerate());
 
-for (let item = 1; item < colors.length; item += 1) {
-  colors[item].style.background = colorGenerate();
-}
+const randomColors = () => {
+  for (let item = 1; item < colors.length; item += 1) {
+    colors[item].style.background = colorGenerate();
+  }
+};
+
+button.addEventListener('click', randomColors);
+
+window.onload = randomColors;
